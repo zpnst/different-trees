524c0c6e0e4f8fcd982e8514979da4c67fe56642
zpnst <turyanskym@gmail.com>
2023-12-23 19:46:44 +0300

    Hurray! It's working!

--- build/CMakeFiles/comparison.dir/flags.make
+++ build/CMakeFiles/comparison.dir/flags.make
@@ -6,5 +6,5 @@ CXX_DEFINES =
 
 CXX_INCLUDES = -I/home/zpnst/Documents/Programming/cpp/Projects/different-trees/include
 
-CXX_FLAGS = 
+CXX_FLAGS = -std=gnu++20
 
--- comparison-of-trees/comparison.cpp
+++ comparison-of-trees/comparison.cpp
@@ -1,10 +1,28 @@
 
+# include <cstdint>
 # include <iostream>
 # include <trees/binary_tree.hpp>
 # include <trees/avl_binary_tree.hpp>
 
 
 int main(int argc, char **argv) {
-    //тут я хочу просто показать разницу между балансом в этих деревьях
+    
+    binary_tree<std::int64_t, std::string> bin_tree;
+    avl_binary_tree<std::int64_t, std::string> avl_bin_tree;
+
+    std::vector<std::int64_t> vector = {100, 50, 150, 25, 55, 175, 125, 300, 1000};
+
+    for (const auto& object : vector) {
+        bin_tree.push_pair(object, std::to_string(object));
+        avl_bin_tree.push_pair(object, std::to_string(object));
+    }
+
+    std::cout << "\nWe have (1, 2, 3), where\n1 - node value\n2 - node height\n3 - node balance\n\n" << std::endl;
+    
+    std::cout << "Binary Tree:" << std::endl;
+    std::cout << bin_tree << "\n" << std::endl;
+
+    std::cout << "AVL Binary Tree:" << std::endl;
+    std::cout << avl_bin_tree << std::endl;
     return 0;
 }
